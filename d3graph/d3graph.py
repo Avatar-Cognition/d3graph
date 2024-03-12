8615d83ce9f51c0dca727f2a18651555d821a18e
Dani Pinyol <dani@avatarcognition.com>
2024-03-12 10:50:36 +0100

    change handler to only local logger
    All handlers are removed from the root logger and replace by a handler which logs with "[d3graph]" prefix. This causes that this prefix appears in logs from other modules. It also interferes with pytest log handles.
    This PR performs the handler replacement only for the local logger.

--- d3graph/d3graph.py
+++ d3graph/d3graph.py
@@ -26,8 +26,12 @@ from packaging import version
 import datazets as dz
 
 logger = logging.getLogger(__name__)
-if not logger.hasHandlers():
-    logging.basicConfig(level=logging.INFO, format='[{asctime}] [{name}] [{levelname}] {msg}', style='{', datefmt='%d-%m-%Y %H:%M:%S')
+for handler in logger.handlers[:]:
+    logger.removeHandler(handler)
+console = logging.StreamHandler()
+formatter = logging.Formatter('[d3graph] %(levelname)s> %(message)s')
+console.setFormatter(formatter)
+logger.addHandler(console)
 
 
 # %%
