27830b5b106cee0f011e71295ac61efeeb31adc5
Dani Pinyol <dani@avatarcognition.com>
2024-03-14 09:15:23 +0100

    2.5.2

--- d3graph/__init__.py
+++ d3graph/__init__.py
@@ -15,16 +15,7 @@ from d3graph.d3graph import (
 
 __author__ = 'Erdogan Tasksen'
 __email__ = 'erdogant@gmail.com'
-
-# Setup root logger
-_logger = logging.getLogger('d3graph')
-_log_handler = logging.StreamHandler()
-_fmt = '[{asctime}] [{name}] [{levelname}] {msg}'
-_formatter = logging.Formatter(fmt=_fmt, style='{', datefmt='%d-%m-%Y %H:%M:%S')
-_log_handler.setFormatter(_formatter)
-_log_handler.setLevel(logging.DEBUG)
-_logger.addHandler(_log_handler)
-_logger.propagate = False
+__version__ = '2.5.2'
 
 
 # module level doc-string
